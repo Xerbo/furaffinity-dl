86c78dc61a0763ee38e5a25a46ab05d3a69e2610
Ikani <ikani@dreamchaos.net>
2021-07-21 00:29:17 -0700

    Add colorama support and more verbose output Clean up argument parser Add page URL to metadata output Create directory tree based on author name Screen author directory names for bad characters Fix pagination

--- furaffinity-dl.py
+++ furaffinity-dl.py
@@ -9,6 +9,9 @@ import http.cookiejar as cookielib
 import re
 import os
 from time import sleep
+from colorama import init, Fore, Style
+
+init(autoreset=True)
 
 '''
 Please refer to LICENSE for licensing conditions.
@@ -24,16 +27,17 @@ You can also log in to FurAffinity in a web browser and load cookies to download
  python3 furaffinity-dl.py -c cookies.txt gallery letodoesart\n
 DISCLAIMER: It is your own responsibility to check whether batch downloading is allowed by FurAffinity terms of service and to abide by them.
 ''')
-parser.add_argument('category', metavar='category', type=str, nargs='?', default='gallery', help='the category to download, gallery/scraps/favorites')
-parser.add_argument('username', metavar='username', type=str, nargs='?', help='username of the furaffinity user')
-parser.add_argument('folder', metavar='folder', type=str, nargs='?', help='name of the folder')
-parser.add_argument('--output', '-o', dest='output', type=str, default='.', help="output directory")
-parser.add_argument('--cookies', '-c', dest='cookies', type=str, default='', help="path to a NetScape cookies file")
-parser.add_argument('--ua', '-u', dest='ua', type=str, default='Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:68.7) Gecko/20100101 Firefox/68.7', help="Your browser's useragent, may be required, depending on your luck")
-parser.add_argument('--start', '-s', dest='start', type=str, default=1, help="page number to start from")
+parser.add_argument('category', metavar='category', nargs='?', default='gallery', help='the category to download, gallery/scraps/favorites')
+parser.add_argument('username', metavar='username', nargs='?', help='username of the furaffinity user')
+parser.add_argument('folder', metavar='folder', nargs='?', help='name of the folder')
+parser.add_argument('--output', '-o', dest='output', default='.', help="output directory")
+parser.add_argument('--cookies', '-c', dest='cookies', default='', help="path to a NetScape cookies file")
+parser.add_argument('--ua', '-u', dest='ua', default='Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:68.7) Gecko/20100101 Firefox/68.7', help="Your browser's useragent, may be required, depending on your luck")
+parser.add_argument('--start', '-s', dest='start', default=1, help="page number to start from")
 parser.add_argument('--dont-redownload', '-d', const='dont_redownload', action='store_const', help="Don't redownload files that have already been downloaded")
 parser.add_argument('--interval', '-i', dest='interval', type=float, default=0, help="delay between downloading pages")
-parser.add_argument('--metadir', '-m', dest='metadir', type=str, default=None, help="directory to put meta files in")
+parser.add_argument('--metadir', '-m', dest='metadir', default=None, help="directory to put meta files in")
+parser.add_argument('--tree', '-t', const='tree', action='store_const', help="split downloads into folders by author")
 
 args = parser.parse_args()
 if args.username is None:
@@ -80,12 +84,12 @@ page_num = args.start
 def download_file(url, fname, desc):
     r = session.get(url, stream=True)
     if r.status_code != 200:
-        print("Got a HTTP {} while downloading; skipping".format(r.status_code))
+        print(Fore.YELLOW + 'Got a ' + Fore.RED + 'HTTP ' + str(r.status_code) + Fore.YELLOW + ' while downloading; skipping')
         return False
 
     total = int(r.headers.get('Content-Length', 0))
     with open(fname, 'wb') as file, tqdm(
-        desc=desc.ljust(40)[:40],
+        desc=desc.ljust(80)[:80],
         total=total,
         miniters=100,
         unit='b',
@@ -96,7 +100,6 @@ def download_file(url, fname, desc):
             size = file.write(data)
             bar.update(size)
     return True
-
 
 # The cursed function that handles downloading
 def download(path):
@@ -115,6 +118,7 @@ def download(path):
     data = {
         'id': int(path.split('/')[-2:-1][0]),
         'filename': filename,
+        'url': page_url,
         'author': s.find(class_='submission-id-sub-container').find('a').find('strong').text,
         'date': s.find(class_='popup_date').attrs.get('title'),
         'title': title,
@@ -130,7 +134,7 @@ def download(path):
         'comments': []
     }
 
-    # Extact tags
+    # Extract tags
     for tag in s.find(class_='tags-row').findAll(class_='tags'):
         data['tags'].append(tag.find('a').text)
 
@@ -152,25 +156,43 @@ def download(path):
         })
 
     url = 'https:{}'.format(image)
-    output_path = os.path.join(args.output, filename)
 
-    if not args.dont_redownload or not os.path.isfile(output_path):
-        if not download_file(url, output_path, data["title"]):
-            return False
+    if args.tree:
+        output_path = os.path.join(args.output, data["author"].strip('.'))
+        metadir_path = os.path.join(args.metadir, data["author"].strip('.'))
     else:
-        print('Skipping "{}", since it\'s already downloaded'.format(data["title"]))
-        return True
+        output_path = os.path.join(args.output)
+        metadir_path = os.path.join(args.metadir)
 
+    if not os.path.isdir(output_path):
+        print(Fore.GREEN + 'Creating directory for: ' + Fore.CYAN + data["author"])
+        os.makedirs(output_path, exist_ok=True)
+        os.makedirs(metadir_path, exist_ok=True)
+
+    output_file = os.path.join(output_path, filename)
+    descstr = Fore.CYAN + data["author"] + Fore.WHITE + ': ' + Fore.MAGENTA + data["title"]
     # Write a UTF-8 encoded JSON file for metadata
-    with open(os.path.join(args.metadir, '{}.json'.format(filename)), 'w', encoding='utf-8') as f:
+    with open(os.path.join(metadir_path, '{}.json'.format(filename)), 'w', encoding='utf-8') as f:
         json.dump(data, f, ensure_ascii=False, indent=4)
+
+    if not args.dont_redownload or not os.path.isfile(output_file):
+        if not download_file(url, output_file, descstr):
+            return False
+        else:
+            sleep(args.interval)
+    else:
+        print(Fore.YELLOW + 'Skipping: ' + descstr + Fore.YELLOW + ', since it\'s already downloaded')
 
     return True
 
 
 # Main downloading loop
 while True:
-    page_url = '{}/{}'.format(gallery_url, page_num)
+    if page_num == 1:
+        page_url = '{}/{}'.format(gallery_url, page_num)
+    else:
+        page_url = '{}/{}/next'.format(gallery_url, page_num)
+
     response = session.get(page_url)
     s = BeautifulSoup(response.text, 'html.parser')
 
@@ -178,9 +200,11 @@ while True:
     if page_num == 1:
         if s.find(class_='loggedin_user_avatar') is not None:
             account_username = s.find(class_='loggedin_user_avatar').attrs.get('alt')
-            print('Logged in as', account_username)
+            print(Fore.GREEN + 'Logged in as ' + Fore.CYAN + account_username)
         else:
-            print('Not logged in, NSFW content is inaccessible')
+            print(Fore.YELLOW + 'Not logged in, NSFW content is inaccessible')
+
+    print(Fore.GREEN + 'Downloading page ' + Fore.CYAN + str(page_num))
 
     # System messages
     if s.find(class_='notice-message') is not None:
@@ -189,22 +213,17 @@ while True:
 
     # End of gallery
     if s.find(id='no-images') is not None:
-        print('End of gallery')
+        print(Fore.GREEN + 'End of gallery')
         break
 
     # Download all images on the page
     for img in s.findAll('figure'):
         download(img.find('a').attrs.get('href'))
-        sleep(args.interval)
 
-    next_button = s.find('button', class_='button standard', text="Next").parent
+    next_button = s.find('a', class_='button standard right', text="Next")
     if next_button is None:
-        print('Unable to find next button')
+        print(Fore.YELLOW + 'Unable to find next button')
         break
+    page_num = next_button.attrs.get('href').split('/')[-2]
 
-    page_num = next_button.attrs['action'].split('/')[-2]
-
-    print('Downloading page', page_num, page_url)
-
-
-print('Finished downloading')
+print(Fore.WHITE + 'Finished downloading')
